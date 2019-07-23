1dfc63a8b45887bde0c29e2467fa10837d3300c5
Matthew Kress <mckress5433@gmail.com>
2019-07-23 17:06:42 -0400

    Initial Commit

---
+++ Multiplayer_TPS/Source/Multiplayer_TPS/Multiplayer_TPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Multiplayer_TPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYER_TPS_API AMultiplayer_TPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
