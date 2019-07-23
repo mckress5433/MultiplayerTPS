ab66fc7587b7e68abb48b091dc588fad08612c55
Matthew Kress <mckress5433@gmail.com>
2019-07-23 19:16:17 -0400

    Added created character class
    Added TPS_Character and TPS_Character_BP
    Made functionality for walking and crouching
    Added TPS_Character_AnimBP
    Added animations for walking and crouching to animbp

---
+++ Multiplayer_TPS/Source/Multiplayer_TPS/Player/Public/TPS_Character.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPS_Character.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+ATPS_Character::ATPS_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->bUsePawnControlRotation = true;
+	CameraBoom->SetupAttachment(RootComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(CameraBoom);
+
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
+}
+
+// Called when the game starts or when spawned
+void ATPS_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATPS_Character::MoveForward(float _value)
+{
+	AddMovementInput(GetActorForwardVector()*_value);
+}
+
+void ATPS_Character::MoveRight(float _value)
+{
+	AddMovementInput(GetActorRightVector()*_value);
+}
+
+void ATPS_Character::CrouchInputPressed()
+{
+	if (bReadyToCrouch) {
+		bReadyToCrouch = false;
+		if (GetCharacterMovement()->IsCrouching()) {
+			UnCrouch();
+		}
+		else {
+			Crouch();
+		}
+	}
+}
+
+void ATPS_Character::CrouchInputReleased()
+{
+	bReadyToCrouch = true;
+}
+
+// Called every frame
+void ATPS_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATPS_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//Movement Input
+	PlayerInputComponent->BindAxis("Move_X", this, &ATPS_Character::MoveForward);
+	PlayerInputComponent->BindAxis("Move_Y", this, &ATPS_Character::MoveRight);
+	//Camera Input
+	PlayerInputComponent->BindAxis("LookUp", this, &ATPS_Character::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookRight", this, &ATPS_Character::AddControllerYawInput);
+	
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ATPS_Character::CrouchInputPressed);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ATPS_Character::CrouchInputReleased);
+}
+
---
+++ Multiplayer_TPS/Source/Multiplayer_TPS/Player/Public/TPS_Character.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TPS_Character.generated.h"
+
+//Forward Declarations 
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class MULTIPLAYER_TPS_API ATPS_Character : public ACharacter
+{
+	GENERATED_BODY()
+
+private:
+	bool bReadyToCrouch = true;
+
+public:
+	// Sets default values for this character's properties
+	ATPS_Character();
+
+protected:
+	/********  Character Components  ********/
+	UPROPERTY(Category = Components, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(Category = Components, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	/********  Protected Functions  ********/
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Movement Functions
+	void MoveForward(float _value);
+	void MoveRight(float _value);
+
+	void CrouchInputPressed();
+	void CrouchInputReleased();
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
